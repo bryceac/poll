da7174962eb02af5b0e72c4b7478241467263ca3
Bryce Campbell <tonyhawk2100@gmail.com>
2025-02-14 15:37:22 -0800

    work on creating the actual form.

--- create.php
+++ create.php
@@ -1,1 +1,19 @@
 <?php include("functions.php"); ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>New Poll</title>
+    </head>
+    <body>
+        <h1>Create Poll</h1>
+        <div id="poll">
+            <form action="" method="post" class="response">
+                <p><label for="question">Enter Poll Prompt: </label>
+                <input type="text" name="question" id="question"></p>
+                <p><label for="answers">Enter Poll Prompt: </label>
+                <input type="text" name="answers" id="answers"></p>
+                <input type="submit" value="Submit">
+            </form>
+        </div>
+    </body>
+</html>
