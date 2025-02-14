d7021aaf79c92db533922894ca68c5e2797209a3
Bryce Campbell <tonyhawk2100@gmail.com>
2025-02-14 15:23:39 -0800

    fix spacing, to make it easier to read.

--- index.php
+++ index.php
@@ -10,24 +10,24 @@
 </head>
 <body>
     <?php if (isset($current_poll)) { ?>
-    <h1><?php echo htmlspecialchars($current_poll->question); ?></h1>
+        <h1><?php echo htmlspecialchars($current_poll->question); ?></h1>
 
-    <div id="poll">
-        <?php if ($current_poll->total_votes() > 0) { ?>
-            <?php foreach($current_poll->answers as $answer) { ?>
-                <div class="response">
-                    <label for="<?php echo $answer->id; ?>"><?php echo htmlspecialchars($answer->value); ?></label>
-                    <meter value="<?php echo $answer->votes; ?>" min="0" max="<?php echo $current_poll->total_votes ?>"></meter>
-                </div>
+        <div id="poll">
+            <?php if ($current_poll->total_votes() > 0) { ?>
+                <?php foreach($current_poll->answers as $answer) { ?>
+                    <div class="response">
+                        <label for="<?php echo $answer->id; ?>"><?php echo htmlspecialchars($answer->value); ?></label>
+                        <meter value="<?php echo $answer->votes; ?>" min="0" max="<?php echo $current_poll->total_votes ?>"></meter>
+                    </div>
+                <?php } ?>
+            <?php } else { ?>
+                <?php foreach($current_poll->answers as $answer) { ?>
+                    <div class="response">
+                        <p><?php echo htmlspecialchars($answer->value); ?></p>
+                    </div>
+                <?php } ?>
             <?php } ?>
-        <?php } else { ?>
-            <?php foreach($current_poll->answers as $answer) { ?>
-                <div class="response">
-                    <p><?php echo htmlspecialchars($answer->value); ?></p>
-                </div>
-            <?php } ?>
-        <?php } ?>
-    </div>
+        </div>
     <?php } else { ?>
         <p>Sorry, There are no polls to display at this time.</p>
     <?php } ?>
