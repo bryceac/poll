423f63b34ddf05d202f4f07ece5c61f7831bc9df
Bryce Campbell <tonyhawk2100@gmail.com>
2025-02-14 15:43:33 -0800

    create basic for form.

--- create.php
+++ create.php
@@ -10,8 +10,8 @@
             <form action="" method="post" class="response">
                 <p><label for="question">Enter Poll Prompt: </label>
                 <input type="text" name="question" id="question"></p>
-                <p><label for="answers">Enter Poll Prompt: </label>
-                <input type="text" name="answers" id="answers"></p>
+                <p><label for="answers">Enter Poll Prompt: </label></p>
+                <textarea name="answers" id="answers" placeholder="Enter options here. One option per line.">
                 <input type="submit" value="Submit">
             </form>
         </div>
