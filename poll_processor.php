6b324b0afd700a34450747fa1f9160f2559053b1
Bryce Campbell <tonyhawk2100@gmail.com>
2025-02-14 16:27:50 -0800

    work on parsing input.

---
+++ poll_processor.php
@@ -0,0 +1,23 @@
+<?php
+include("functions.php");
+
+$question = $_POST["question"];
+$options = $_POST["answers"];
+$poll = new Poll();
+
+function answers_from($input = "") {
+    $answers = [];
+
+    $input_array = preg_split("/\r\n|\n|\r/", $input);
+
+    foreach ($input_array as $response) {
+        $answer = new Answer();
+
+        $answer->value = $response;
+        $answers[] = $answer;
+    }
+
+
+    return $answers;
+}
+?>
