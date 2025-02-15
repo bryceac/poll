42d16b2526a8cf1821f07b2af8356158d2f80511
Bryce Campbell <tonyhawk2100@gmail.com>
2025-02-14 17:43:46 -0800

    try to get vote added to count.

--- vote.php
+++ vote.php
@@ -8,7 +8,12 @@ $polls = retrieve_polls();
 $current_poll = empty($polls) ? null : $polls[count($polls) -1];
 
 if (isset($current_poll)) {
-    
+    foreach ($current_poll->answers as $answer) {
+        if (str_contains(strtolower($vote), strtolower($answer->value))) {
+            $answer->value += 1;
+            break;
+        }
+    }
 } else {
     die("no poll found!");
 }
