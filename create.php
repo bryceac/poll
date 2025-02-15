c99e27a88581942fe22f9cd3f78b89cf26d84b2a
Bryce Campbell <tonyhawk2100@gmail.com>
2025-02-14 16:02:52 -0800

    move code back to main page.

--- create.php
+++ create.php
@@ -3,16 +3,17 @@
 <html>
     <head>
         <title>New Poll</title>
+        <link rel="stylesheet" href="form.css" media="all">
     </head>
     <body>
         <h1>Create Poll</h1>
-        <div id="poll">
+        <div id="poll_form">
             <form action="" method="post" class="response">
                 <p><label for="question">Enter Poll Prompt: </label>
                 <input type="text" name="question" id="question"></p>
-                <p><label for="answers">Enter Poll Prompt: </label></p>
-                <textarea name="answers" id="answers" placeholder="Enter options here. One option per line.">
-                <input type="submit" value="Submit">
+                <p><label for="answers">Enter Responses: </label></p>
+                <p><textarea name="answers" id="answers" placeholder="Enter options here. One option per line." rows="32"></textarea></p>
+                <p><input type="submit" value="Submit"></p>
             </form>
         </div>
     </body>
