17483e876f521e02e7aa3f6ad7b8d30403c424c4
Bryce Campbell <tonyhawk2100@gmail.com>
2025-02-15 09:01:07 -0800

    start work on establishing database connections.

--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,11 @@
 <?php
 class DB {
-    
+    private $host = "../assets/polls.db";
+
+    function connect() {
+        $conn = new PDO("sqlite:realpath($host)");
+
+        return $conn;
+    }
 }
 ?>
