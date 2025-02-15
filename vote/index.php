3c94c7913e964626086733ab507442d89af715a7
Bryce Campbell <tonyhawk2100@gmail.com>
2025-02-14 18:14:38 -0800

    make sure things work properly.

--- vote/index.php
+++ vote/index.php
@@ -19,6 +19,12 @@ if (isset($current_poll)) {
 
     if (isset($stored_answer)) {
         $stored_answer->votes += 1;
+
+        update_vote_in_database($stored_answer);
+
+        echo("Thanks for voting!");
+    } else {
+        die("choice was not valid!");
     }
 } else {
     die("no poll found!");
