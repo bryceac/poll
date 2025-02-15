d2230204ae293fe25b9110709b40d9e44a793984
Bryce Campbell <tonyhawk2100@gmail.com>
2025-02-14 17:46:44 -0800

    keep working on getting voting worked on.

--- vote.php
+++ vote.php
@@ -7,12 +7,18 @@ $polls = retrieve_polls();
 
 $current_poll = empty($polls) ? null : $polls[count($polls) -1];
 
+$stored_answer = NULL;
+
 if (isset($current_poll)) {
     foreach ($current_poll->answers as $answer) {
         if (str_contains(strtolower($vote), strtolower($answer->value))) {
-            $answer->value += 1;
+            $stored_answer = $answer;
             break;
         }
+    }
+
+    if (isset($stored_answer)) {
+        $stored_answer->value += 1;
     }
 } else {
     die("no poll found!");
