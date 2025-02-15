7354a8e18ab24ac4a7a3d10f90d342f5eaabf665
Bryce Campbell <tonyhawk2100@gmail.com>
2025-02-14 17:36:36 -0800

    start working on taking votes.

--- vote.php
+++ vote.php
@@ -2,4 +2,14 @@
 include("functions.php");
 
 $vote = $_GET["choice"];
+
+$polls = retrieve_polls();
+
+$current_poll = empty($polls) ? null : $polls[count($polls) -1];
+
+if (isset($current_poll)) {
+    
+} else {
+    die("no poll found!");
+}
 ?>
