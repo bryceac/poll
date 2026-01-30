e924e463338a4591cc7cf9759d80113f10c55c1c
Bryce Campbell <tonyhawk2100@gmail.com>
2026-01-30 10:39:38 -0800

    add in code to close poll.

--- close/index.php
+++ close/index.php
@@ -5,5 +5,9 @@ $db = new DB();
 
 $current_poll = isset($_GET["id"]) ? $db->retrieve_poll_with_id($_GET["id"]) : $db->current_poll();
 
-
+if ($current_poll->is_open) {
+    $db->close_poll($current_poll);
+} else {
+    die("Poll is already closed.");
+}
 ?>
