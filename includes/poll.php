6dbf3d0cee0ba43c332808a5b12956b2191f16f8
Bryce Campbell <tonyhawk2100@gmail.com>
2026-01-30 09:47:39 -0800

    add way to determine is poll is open.

--- includes/poll.php
+++ includes/poll.php
@@ -3,16 +3,19 @@ class Poll {
     public $id;
     public $question;
     public $answers;
+    public $is_open;
 
     function __construct($array = []) {
         if (empty($array)) {
             $this->id = 0;
             $this->question = "";
             $this->answers = [];
+            $this->is_open = true;
         } else {
             $this->id = $array["id"];
             $this->question = $array["question"];
             $this->answers = [];
+            $this->is_open = strcasecmp($array["open"], "Y") == 0 ? true : false;
         }
     }
 
