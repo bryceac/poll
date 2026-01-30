88176ca6ea4a1d96c5e33915db8d90ed78500add
Bryce Campbell <tonyhawk2100@gmail.com>
2026-01-30 10:13:05 -0800

    make sure closed polls do not accept votes.

--- vote/index.php
+++ vote/index.php
@@ -10,22 +10,27 @@ $current_poll = $db->current_poll();
 $stored_answer = NULL;
 
 if (isset($current_poll)) {
-    foreach ($current_poll->answers as $answer) {
-        if (str_contains(strtolower($vote), strtolower($answer->value))) {
-            $stored_answer = $answer;
-            break;
+    if ($current_poll->is_open) {
+        foreach ($current_poll->answers as $answer) {
+            if (str_contains(strtolower($vote), strtolower($answer->value))) {
+                $stored_answer = $answer;
+                break;
+            }
         }
-    }
-
-    if (isset($stored_answer)) {
-        $stored_answer->votes += 1;
-
-        $db->update_vote_in_database($stored_answer);
-
-        echo("Thanks for voting!");
+    
+        if (isset($stored_answer)) {
+            $stored_answer->votes += 1;
+    
+            $db->update_vote_in_database($stored_answer);
+    
+            echo("Thanks for voting!");
+        } else {
+            die("choice was not valid!");
+        }
     } else {
-        die("choice was not valid!");
+        die("Sorry, poll is no longer open for voting.");
     }
+    
 } else {
     die("no poll found!");
 }
